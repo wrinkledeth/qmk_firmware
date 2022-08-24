499428937e61d2c61674efb4c0729a2f4472e276
wrinkled <wrinkledeth@gmail.com>
2022-08-23 23:32:18 -0400

    ultimate changesls!

--- keyboards/handwired/dactyl_promicro/keymaps/mike/keymap.c
+++ keyboards/handwired/dactyl_promicro/keymaps/mike/keymap.c
@@ -7,28 +7,30 @@
 #define RAISE MO(_RAISE)
 // #define LOWER MO(_LOWER)
 
+// https://github.com/qmk/qmk_firmware/blob/master/docs/feature_advanced_keycodes.md
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_6x6(
         KC_GRV , KC_1  ,KC_2   , KC_3  , KC_4  , KC_5  ,            KC_6   , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
         KC_ESC , KC_Q  ,KC_W   , KC_F  , KC_P  , KC_B  ,            KC_J   , KC_L  , KC_U  , KC_Y  , KC_SCLN ,KC_BSLS,
         KC_TAB , KC_A  ,KC_R   , KC_S  , KC_T  , KC_G  ,            KC_M   , KC_N  , KC_E  , KC_I  ,KC_O, KC_QUOT,
         KC_F12  , KC_Z  ,KC_X   , KC_C  , KC_D  , KC_V  ,           KC_K   , KC_H  ,KC_COMM, KC_DOT,KC_SLSH,KC_EQL,
-        RAISE,  KC_LGUI, KC_SLSH, KC_LEFT, KC_RGHT,_______,         _______ , KC_DOWN ,KC_UP,KC_LBRC,KC_RBRC,RAISE,
-                                RAISE, KC_LCTL, RAISE,                KC_3, KC_ENT,KC_DEL, 
-                                                KC_2,               KC_4,
+        RAISE,  KC_LGUI, LCTL(KC_SLSH), KC_LEFT, KC_RGHT,_______,   _______ , KC_DOWN ,KC_UP,KC_LBRC,KC_RBRC,KC_F2,
+                                KC_LALT, KC_LCTL, KC_HOME,          KC_PGDOWN, KC_ENT,KC_DEL, 
+                                                KC_END,             KC_PGUP,    
                                                 KC_SPC,             KC_BSPC, 
-                                                KC_LALT,            KC_RSHIFT   
+                                                RAISE,              KC_RSHIFT   
     ),  
     [_RAISE] = LAYOUT_6x6(
-        KC_F12 ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,        KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,
-        RESET,_______,_______,_______,KC_PSCR,_______,        _______,_______,KC_PGUP,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,        KC_HOME,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_END,
-        _______,_______,_______,_______,_______,_______,        _______,KC_PGDOWN,KC_PGDOWN,_______,_______,_______,
-        _______,_______,_______,_______,_______,_______,        _______,_______,_______,_______,_______,_______,
-                                _______,_______,_______,        _______,_______,_______,
-                                                _______,        _______,
-                                                KC_LSFT,        _______,
-                                                _______,        _______
+        KC_F12 ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,            KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,
+        _______,_______,_______,_______,KC_PSCR,_______,            _______,_______,KC_PGUP,_______,_______,RESET,
+        LALT(LCTL(KC_TAB)),_______,_______,_______,KC_LCTL,_______, KC_HOME,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_END,
+        _______,_______,_______,_______,_______,_______,            _______,KC_PGDOWN,KC_PGDOWN,_______,_______,_______,
+        _______,_______,_______,_______,_______,_______,            _______,_______,_______,_______,_______,_______,
+                                _______,_______,_______,            _______,_______,_______,
+                                                _______,            _______,
+                                                KC_LSFT,            _______,    
+                                                _______,            _______
     ),
     // [_LOWER] = LAYOUT_6x6(
     //     _______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,                                       KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,
