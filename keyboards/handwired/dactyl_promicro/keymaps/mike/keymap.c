165f5a39c3165aea5d72575440ce1300e93b5ee4
wrinkled <wrinkledeth@gmail.com>
2022-09-09 08:49:33 -0400

    latest changed

--- keyboards/handwired/dactyl_promicro/keymaps/mike/keymap.c
+++ keyboards/handwired/dactyl_promicro/keymaps/mike/keymap.c
@@ -20,7 +20,8 @@
 enum {
     TD_TAB,
     TD_QUIT,
-    // TD_RAISE
+    TD_PASTE,
+    TD_B,
 };
 
 // Tap Dance Definitions
@@ -28,16 +29,14 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     // simple tap dance
     [TD_TAB] = ACTION_TAP_DANCE_DOUBLE(LALT(KC_TAB), LCTL(LALT(KC_TAB))), // alt tab / ctrl + alt + tab
     [TD_QUIT] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, LALT(KC_F4)), // esc / alt + f4 
-    // [TD_RAISE] = ACTION_TAP_DANCE_DOUBLE(MO(_RAISE), TG(_RAISE)), 
+    [TD_PASTE] = ACTION_TAP_DANCE_DOUBLE(LCTL(KC_V), LALT(LCTL(KC_V))), //paste / markdown paste
+    [TD_B] = ACTION_TAP_DANCE_DOUBLE(LCTL(KC_B), LALT(LCTL(KC_B))), //open sidebar / markdown bold
+
     // To get Tap Dance Raise to work you would need to do this: 
     // https://github.com/qmk/qmk_firmware/blob/master/docs/feature_tap_dance.md (ex 6)
-
     // complex tap dance function (to specify what happens when key is pressed 3+ times, for example). See full docs for how to define
     // [YOUR_TAPDANCE_2] = ACTION_TAP_DANCE_FN(your_function_name),iiii
 };
-
-// ctrl + up / down, make it skip to next paragraph
-// map scrolllineup/down to ctrl+shift+up/down 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_6x6(
@@ -52,10 +51,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 RAISE,                          KC_RSHIFT   
     ),              
     [_RAISE] = LAYOUT_6x6(            
-        KC_F12 ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,                        KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,
-        TD(TD_QUIT),_______,_______,_______,KC_PSCR,_______,                    _______,_______,KC_PGDOWN,KC_PGUP,_______,_______,
-        TD(TD_TAB),KC_LGUI,KC_LALT,KC_LSFT,KC_LCTL,_______,                     KC_HOME,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_END,
-        _______,LCTL(KC_Z),LCTL(KC_X),LCTL(KC_C),MOUSE,LCTL(KC_V),              _______,_______,KC_WH_D,KC_WH_U,_______,_______,
+        KC_F12 ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,                
+                KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,
+        TD(TD_QUIT),_______,_______,_______,KC_PSCR,TD(TD_B),                    _______,KC_HOME,KC_PGDOWN,KC_PGUP,KC_END,_______,
+        TD(TD_TAB),KC_LGUI,KC_LALT,KC_LSFT,KC_LCTL,MOUSE,                       KC_HOME,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_END,
+        _______,LCTL(KC_Z),LCTL(KC_X),LCTL(KC_C),LCTL(KC_D),TD(TD_PASTE),         _______,KC_WH_L,KC_WH_D,KC_WH_U,KC_WH_R,_______,
         _______,_______,LCTL(KC_W),LCTL(KC_PGUP) ,LCTL(KC_PGDOWN),_______,      _______,_______,_______,_______,_______,_______,
                                 _______,_______,_______,                        _______,_______,_______,
                                                 _______,                        _______,
